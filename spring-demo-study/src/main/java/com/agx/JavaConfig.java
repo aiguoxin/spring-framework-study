e688acf46cca900ea6a77ce1e45bcf8f372af9ec
艾国信 <aiguoxin@qiyi.com>
2022-07-14 15:57:22 +0800

    debug 注解配置

--- spring-demo-study/src/main/java/com/agx/JavaConfig.java
+++ spring-demo-study/src/main/java/com/agx/JavaConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
  * 2020/6/22 下午3:31
  * aiguoxin
  * 说明:
- * @Configuration作用相当于xml中的<beans><beans/>节点
+ * @Configuration作用相当于xml中的<beans><beans/>节点，通过ConfigurationClassPostProcessor处理
  * @Bean相当于xml中<bean></bean>节点
  * @ComponentScan(basePackages = "com.agx") 扫描com.agx下的@Component实体实例化，所以注入bean，可以采用@Bean和扫描方式
  */
